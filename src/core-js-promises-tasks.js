446950f4b34bab81c006e219a027bd1b8675a6ff
Michail <astapenko.m99@gmail.com>
2025-04-12 23:49:54 +0300

    feat: all tasks comlited

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -163,8 +163,13 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+
+function queuePromises(promises) {
+  return promises.reduce((acc, promise) => {
+    return acc.then((result) => {
+      return promise.then((value) => result + String(value));
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
